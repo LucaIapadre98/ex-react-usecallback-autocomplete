e4f3b48b1a4f575e3575d566fbdc57c049c9e837
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-07-31 12:12:56 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-//  Premessa: Stai sviluppando un campo di ricerca intelligente simile a quello di Amazon. 
-//  Quando l'utente digita, una tendina di suggerimenti mostra i prodotti corrispondenti alla ricerca. 
-//  Per evitare richieste API eccessive, devi ottimizzare la ricerca con il debounce.
-
-// Crea un campo di input (<input type="text">) in cui l’utente può digitare.
-// Effettua una chiamata API a: 
-// /products?search=[query]
-// La query deve essere sostituita con il testo digitato.
-// Mostra i risultati API sotto l'input in una tendina di suggerimenti.
-// Se l'utente cancella il testo, la tendina scompare.
-// Obiettivo: Mostrare suggerimenti dinamici in base alla ricerca dell'utente.
-
-// Attualmente, ogni pressione di tasto esegue una richiesta API. 
-// Questo è inefficiente!
-// Implementa una funzione di debounce per ritardare la chiamata API 
-// fino a quando l’utente smette di digitare per un breve periodo (es. 300ms)
-// Dopo l’implementazione, verifica che la ricerca non venga eseguita immediatamente a ogni tasto premuto, ma solo dopo una breve pausa.
-// Obiettivo: Ridurre il numero di richieste API e migliorare le prestazioni.
-
 const debounce = (callback, delay) =>{
-  let timer;
+  let timeout;
   return (value) => {
     clearTimeout(timeout);
     timeout = setTimeout(() => {
@@ -74,3 +55,23 @@ function App() {
 };
 
 export default App;
+
+
+//  Premessa: Stai sviluppando un campo di ricerca intelligente simile a quello di Amazon. 
+//  Quando l'utente digita, una tendina di suggerimenti mostra i prodotti corrispondenti alla ricerca. 
+//  Per evitare richieste API eccessive, devi ottimizzare la ricerca con il debounce.
+
+// Crea un campo di input (<input type="text">) in cui l’utente può digitare.
+// Effettua una chiamata API a: 
+// /products?search=[query]
+// La query deve essere sostituita con il testo digitato.
+// Mostra i risultati API sotto l'input in una tendina di suggerimenti.
+// Se l'utente cancella il testo, la tendina scompare.
+// Obiettivo: Mostrare suggerimenti dinamici in base alla ricerca dell'utente.
+
+// Attualmente, ogni pressione di tasto esegue una richiesta API. 
+// Questo è inefficiente!
+// Implementa una funzione di debounce per ritardare la chiamata API 
+// fino a quando l’utente smette di digitare per un breve periodo (es. 300ms)
+// Dopo l’implementazione, verifica che la ricerca non venga eseguita immediatamente a ogni tasto premuto, ma solo dopo una breve pausa.
+// Obiettivo: Ridurre il numero di richieste API e migliorare le prestazioni.
